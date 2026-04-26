d184f8c44a4b2ce7b9e62075be38fb3ce008508b
MemeMachine420x <therealdanny1@gmail.com>
2026-04-26 21:07:51 +0000

    changes

--- spark_tts_api.py
+++ spark_tts_api.py
@@ -2,28 +2,37 @@ from flask import Flask, request, Response
 from gradio_client import Client, handle_file
 import subprocess
 import os
+import traceback
 
 app = Flask(__name__)
 voicePath = "/home/rangernet/spark-ha-component/voices/lisa.wav"
 
 @app.route('/tts', methods=['POST'])
 def tts():
-    requestText = request.json.get('text')
-    print(requestText)
-    client = Client("http://192.168.100.74:7860/")
-    result = client.predict(
-		text=requestText,
-		prompt_text="",
-		prompt_wav_upload=handle_file(voicePath),
-		prompt_wav_record=handle_file(voicePath),
-		api_name="/voice_clone"
-)
-    print(result)
-    proc = subprocess.run(
-        ['ffmpeg', '-hide_banner', '-loglevel', 'error', '-i', result, '-f', 'mp3', 'pipe:1'],
-        capture_output=True
-    )
-    return Response(proc.stdout, mimetype='audio/mpeg')
+    try:
+        requestText = request.json.get('text')
+        print(requestText)
+        client = Client("http://192.168.100.74:7860/")
+        result = client.predict(
+            text=requestText,
+            prompt_text="",
+            prompt_wav_upload=handle_file(voicePath),
+            prompt_wav_record=handle_file(voicePath),
+            api_name="/voice_clone"
+        )
+        print(result)
+        ffmpeg = '/usr/bin/ffmpeg'
+        proc = subprocess.run(
+            [ffmpeg, '-hide_banner', '-loglevel', 'error', '-i', result, '-f', 'mp3', 'pipe:1'],
+            capture_output=True
+        )
+        if proc.returncode != 0:
+            print("ffmpeg error:", proc.stderr.decode())
+            return Response("ffmpeg failed", status=500)
+        return Response(proc.stdout, mimetype='audio/mpeg')
+    except Exception as e:
+        traceback.print_exc()
+        return Response(str(e), status=500)
 
 @app.route('/voice', methods=['POST'])
 def voice():
